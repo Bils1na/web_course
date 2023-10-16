6046d5a861bbfc62220da09b791d6520145f5d26
Artemiy Pilipchuk <100224557+Bils1na@users.noreply.github.com>
2023-10-16 17:06:29 +0000

    task2

--- seminarIII/main.js
+++ seminarIII/main.js
@@ -12,6 +12,22 @@
 // которые ввел пользователь, вывод должен выглядеть так (пример):
 // Результат сложения числе 5 и 2 равен 7.
 
-const a = parseInt(prompt("Enter first number:"));
-const b = parseInt(prompt("Enter second number:"));
-alert(`Sum of numbers ${a} and ${b} = ${a+b}`);
+// function sum(n1, n2) {
+//     const res = n1 + n2;
+//     return res;
+// }
+
+// const a = Number.parseFloat(prompt("Enter first number:"));
+// const b = Number.parseFloat(prompt("Enter second number:"));
+// // alert(`Sum of numbers ${a} and ${b} = ${a+b.toFixed(2)}`);
+// const roundedSum = Math.round(sum(a, b) * 100) / 100;
+// alert(`Sum of numbers ${a} and ${b} = ${roundedSum}`);
+
+// Задача 2: вывести на экран в диалоговом окне текст с сообщением “Вам хорошо живется?”
+// и кнопками «ОК», «Отмена», для чего необходимо использовать confirm.
+// - При нажатии на кнопку “ОК” вывести в диалоговом окне текст с сообщением 
+// “Тогда мы идем к вам!”.
+// - При нажатии на кнопку “Отмена” вывести в диалоговом окне текст с сообщением 
+// “Ну, вы держитесь там!”.
+
+const answer = (confirm("Do you live well?") == 1) ? "So we're going to you!" : "Well, be strong!";

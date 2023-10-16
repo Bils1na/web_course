9eb9ab65e8daea7b55b75225fba280eb43d1d884
Artemiy Pilipchuk <100224557+Bils1na@users.noreply.github.com>
2023-10-16 16:41:40 +0000

    semIII

---
+++ seminarIII/main.js
@@ -0,0 +1,17 @@
+// let num = 5;
+// alert(num);
+
+// num = 10;
+// alert(num);
+
+// const num1 = 10;
+
+// console.log(num, num1);
+
+// Задача 1: Получить от пользователя два числа и вывести в диалоговое окно сумму значений,
+// которые ввел пользователь, вывод должен выглядеть так (пример):
+// Результат сложения числе 5 и 2 равен 7.
+
+const a = parseInt(prompt("Enter first number:"))
+const b = parseInt(prompt("Enter second number:"))
+alert(`Sum of numbers $(a) and $(b) = $(a+b)`)
